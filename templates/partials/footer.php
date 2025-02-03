6a90815c630d464b9361f6e2343909b5b3afebc4
Anthony <anthony.arrighi@orange.fr>
2025-02-03 09:24:23 +0100

    feat: base intégration maquette + page add-book

---
+++ templates/partials/footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="container">
+        <ul>
+            <li>
+                <a href="#">
+                    Politique de confidentialité
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Mentions légales
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Tom Troc©
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="./build/images/logo_min.png" alt="TomTroc">
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
---
+++ templates/partials/header.php
@@ -0,0 +1,41 @@
+<header>
+    <div class="container">
+        <a href="#">
+            <img src="./build/images/logo.svg" alt="TomTroc">
+        </a>
+        <div class="links">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#">Nos livres à l'échange</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="user_menu">
+                <ul>
+                    <li>
+                        <a href="#" class="icon">
+                            <i class="fa-regular fa-comment"></i>
+                            Messagerie
+                            <span class="badge">1</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="icon">
+                            <i class="fa-regular fa-user"></i>
+                            Mon compte
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Connexion
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
