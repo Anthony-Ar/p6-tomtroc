01f6ea837d81db0f97f24b9fc74e2dc3cd3ef071
Anthony <anthony.arrighi@orange.fr>
2025-02-05 16:23:07 +0100

    feat: début intégration homepage

---
+++ templates/pages/home/home.php
@@ -0,0 +1,50 @@
+<main>
+    <?= include_once 'partials/section-discover.php'; ?>
+    <section class="container">
+        <div class="section__wrapper">
+            <h1 class="fs-36 pb-5">Les derniers livres ajoutés</h1>
+
+            <div class="card__grid">
+                <article class="card enlarge-link">
+                    <img src="build/images/book-cover/cover1.png" alt="Cover livre n°1" />
+                    <div class="card__content">
+                        <a href="#" class="card__content--title">Esther</a>
+                        <p class="card__content--description">Alabaster</p>
+                        <p class="card__content--seller">Vendu par : CamilleClubLit</p>
+                    </div>
+                </article>
+
+                <article class="card enlarge-link">
+                    <img src="build/images/book-cover/cover2.png" alt="Cover livre n°2" />
+                    <div class="card__content">
+                        <a href="#" class="card__content--title">The Kinkfolk Table</a>
+                        <p class="card__content--description">Nathan Williams</p>
+                        <p class="card__content--seller">Vendu par : Nathalire</p>
+                    </div>
+                </article>
+
+                <article class="card enlarge-link">
+                    <img src="build/images/book-cover/cover3.png" alt="Cover livre n°3" />
+                    <div class="card__content">
+                        <a href="#" class="card__content--title">Wabi Sabi</a>
+                        <p class="card__content--description">Beth Kempton</p>
+                        <p class="card__content--seller">Vendu par : Alexlecture</p>
+                    </div>
+                </article>
+
+                <article class="card enlarge-link">
+                    <img src="build/images/book-cover/cover4.png" alt="Cover livre n°4" />
+                    <div class="card__content">
+                        <a href="#" class="card__content--title">Milk & honey</a>
+                        <p class="card__content--description">Rupi Kaur</p>
+                        <p class="card__content--seller">Vendu par : Hugo1990_12</p>
+                    </div>
+                </article>
+            </div>
+
+            <a href="#" class="btn m-auto">
+                Voir tous les livres
+            </a>
+        </div>
+    </section>
+</main>
