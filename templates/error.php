d23a3a3145076c9d1e6ac63becf705b8babb5dce
Anthony <anthony.arrighi@orange.fr>
2025-02-17 19:35:39 +0100

    fix: error.php template (add container)

--- templates/error.php
+++ templates/error.php
@@ -1,16 +1,18 @@
 <main>
-    <div class="breadcrumbs">
-        <a href="/" class="icon">
-            <i class="fa-solid fa-arrow-left"></i>
-            Revenir sur l'accueil de TomTroc
-        </a>
-    </div>
-
-    <section>
-        <h1>Oops... Une erreur est survenue.</h1>
-        <div class="error_content">
-            <h2>Erreur <?= $codeError ?></h2>
-            <p class="error_description"><?= $messageError ?></p>
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/" class="icon">
+                <i class="fa-solid fa-arrow-left"></i>
+                Revenir sur l'accueil de TomTroc
+            </a>
         </div>
-    </section>
+
+        <section>
+            <h1>Oops... Une erreur est survenue.</h1>
+            <div class="error_content">
+                <h2>Erreur <?= $codeError ?></h2>
+                <p class="error_description"><?= $messageError ?></p>
+            </div>
+        </section>
+    </div>
 </main>
