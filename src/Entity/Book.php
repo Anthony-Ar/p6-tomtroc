885dd8be8a52187d5198f284e39dbb540b8dc155
Anthony <anthony.arrighi@orange.fr>
2025-03-01 10:25:27 +0100

    style: code styling phpstorm

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -6,11 +6,11 @@ namespace App\Entity;
 
 class Book extends MainEntity
 {
-    public int $id;
+    public int    $id;
     public string $title;
     public string $author;
     public string $description;
     public string $cover;
-    public bool $state;
-    public int $ownerId;
+    public bool   $state;
+    public int    $ownerId;
 }
