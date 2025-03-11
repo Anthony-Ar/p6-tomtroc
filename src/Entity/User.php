4a59e9f62de187790643d8973a781455807f16c1
Anthony <anthony.arrighi@orange.fr>
2025-03-11 18:22:12 +0100

    feat: migration entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,14 @@ declare(strict_types = 1);
 
 namespace App\Entity;
 
+use DateTime;
+
 class User extends MainEntity
 {
-    public int    $id;
-    public string $username;
-    public string $email;
-    public string $password;
+    public int      $id;
+    public string   $username;
+    public string   $email;
+    public string   $password;
+    public DateTime $inscriptionDate;
+    public string   $avatar;
 }
