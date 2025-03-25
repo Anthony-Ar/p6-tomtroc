19a030f39604c5f3f5f451d34b67044fa8ad92a6
Anthony <anthony.arrighi@orange.fr>
2025-03-25 14:24:14 +0100

    style: début intégration page messagerie

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
+use App\Framework\Exception\ViewNotFoundException;
+
 class MessageController extends MainController
 {
-
+    /**
+     * Affiche l'accueil de la messagerie
+     * @return void
+     * @throws ViewNotFoundException
+     */
+    public function showMessenger() : void
+    {
+        $this->render('Messagerie', 'pages/message/messenger');
+    }
 }
---
+++ templates/pages/message/messenger.php
@@ -0,0 +1,19 @@
+<main class="container">
+    <section class="messenger">
+        <div class="messenger__panel">
+            <h1>Messagerie</h1>
+
+            <article class="discussion active">
+                <img src="/build/images/avatar/avatar.png" alt="Avatar de">
+                <div class="discussion__detail">
+                    <div class="discussion__detail--infos">
+                        <p>Alexlecture</p>
+                        <p class="date">15:43</p>
+                    </div>
+                    <p class="description">Lorem ipsum dolor sit amet,...</p>
+                </div>
+            </article>
+
+        </div>
+    </section>
+</main>
