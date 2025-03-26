b9211f47a5cf673c5775cb1efddc621a5043356e
Anthony <anthony.arrighi@orange.fr>
2025-03-26 09:41:49 +0100

    feat: intégration maquette Messagerie

--- templates/pages/message/messenger.php
+++ templates/pages/message/messenger.php
@@ -3,7 +3,7 @@
         <div class="messenger__panel">
             <h1>Messagerie</h1>
 
-            <article class="discussion active">
+            <div class="discussion active">
                 <img src="/build/images/avatar/avatar.png" alt="Avatar de">
                 <div class="discussion__detail">
                     <div class="discussion__detail--infos">
@@ -12,8 +12,41 @@
                     </div>
                     <p class="description">Lorem ipsum dolor sit amet,...</p>
                 </div>
-            </article>
+            </div>
+        </div>
 
+        <div class="messenger__discussion">
+            <div class="messenger__discussion--head">
+                <img src="/build/images/avatar/avatar.png" alt="Avatar de">
+                <p>Alexlecture</p>
+            </div>
+            <div class="messenger__discussion--messages">
+                <div class="message-to">
+                    <p class="date">21.08 15:44</p>
+                    <p class="message">
+                        Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor
+                    </p>
+                </div>
+                <div class="message-from">
+                    <div class="infos">
+                        <img src="/build/images/avatar/avatar.png" alt="Avatar de">
+                        <p class="date">21.08 15:48</p>
+                    </div>
+                    <p class="message">
+                        Lorem ipsum dolor sit amet, consectetur .adipiscing elit, sed do eiusmod tempor
+                    </p>
+                </div>
+
+            </div>
+            <div class="messenger__discussion--sending">
+                <form action="POST">
+                    <div class="input_group">
+                    <label for="content" class="sr-only">Message</label>
+                    <input type="text" name="content" id="content" placeholder="Tapez votre message ici">
+                    </div>
+                    <input type="submit" name="send-message" id="send-message" class="btn" value="Envoyer">
+                </form>
+            </div>
         </div>
     </section>
 </main>
